76c1d4428603efb0e937fda81531a14861392ec4
Hasan Diwan <hd1@jsc.d8u.us>
2021-10-11 14:15:59 -0700

    add factories for rides and users

--- spec/factories/rides.rb
+++ spec/factories/rides.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :ride do
-    source_latitude { "MyString" }
-    source_longitude { "MyString" }
-    target_latitude { "MyString" }
-    target_longitude { "MyString" }
+      source_latitude { "34.0658998" }
+      source_longitude { "-118.374461" }
+      target_latitude { "34.0658998" }
+      target_longitude { "-118.374461" }
+      user_id { User.create!(FactoryBot.attributes_for(:user)).id }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
-FactoryBot.define do
-  factory :user do
-    
-  end
+# User.create!({:email => "guy@gmail.com", :roles => ["admin"], :password => "111111", :password_confirmation => "111111" })
+FactoryBot.define do 
+    factory :user do
+        email { "user@example.com" }
+        password { "password" }
+        password_confirmation { password }
+    end
 end
