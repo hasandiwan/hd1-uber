eff6e3711d052e44a5f353fdd1e56888dfa2f8b8
Hasan Diwan <hd1@jsc.d8u.us>
2021-10-11 16:16:20 -0700

    tests have no syntax errors

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,6 @@
 class Ride < ApplicationRecord
+    validates :source_latitude, numericality: {in: -90..90, message: 'source_latitude out of range'}, on: :create
+    validates :target_latitude, numericality: {in: -90..90, message: 'target_latitude out of range', on: :create}
+    validates :source_longitude, numericality: {in: -180..180, message: 'source_longitude out of range'}, on: :create
+    validates :target_longitude, numericality: {in: -180..180, message: 'target_longitude out of range'}, on: :create
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -2,24 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Ride, type: :model do
     describe 'Ride parameters within range' do
-        it 'raises RangeException if source_latitude is not within [-90,90]' do
-            r = create(:ride, source_latitude: 91)
-            expect {r.save}.to raise_error('source_latitude is out of range')
+        it 'raises Exception if source_latitude is not within [-90,90]' do
+            r = create(:ride, source_latitude: '91')
+            r.validate
+            expect((r.errors)).to include('source_latitude is out of range')
         end
 
-        it 'raises RangeException if target_latitude is not within [-90,90]' do
-            r = create(:ride, target_latitude: 91)
-            expect {r.save}.to raise_error('target_latitude is out of range')
+        it 'raises Exception if target_latitude is not within [-90,90]' do
+            r = create(:ride, target_latitude: '91')
+            r.validate
+            expect((r.errors)).to include('target_latitude is out of range')
         end                                                                                                                                                                             
 
-        it 'raises RangeException if source_longitude is not within [-180,180]' do
-            r = create(:ride, source_longitude: 181)
-            expect {r.save}.to raise_error('source_longitude is out of range')
+        it 'raises Exception if source_longitude is not within [-180,180]' do
+            r = create(:ride, source_longitude: '181')
+            r.validate
+            expect((r.errors)).to include('source_longitude is out of range')
         end                                                                                                                                                                             
 
-        it 'raises RangeException if target_longitude is not within [-180,180]' do
-            r = create(:ride, target_longitude: 181)
-            expect {r.save}.to raise_error('target_longitude is out of range')
+        it 'raises Exception if target_longitude is not within [-180,180]' do
+            r = create(:ride, target_longitude: '181')
+            r.validate
+            expect((r.errors)).to include('target_longitude is out of range')
         end                                                                                                                                                                             
     end
 end
