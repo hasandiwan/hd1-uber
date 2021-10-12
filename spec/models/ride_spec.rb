7894cacd9a36ad0878e6f5f86f52d18ee6f20818
Hasan Diwan <hd1@jsc.d8u.us>
2021-10-12 16:01:34 -0700

    fix tests

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -8,16 +8,16 @@ RSpec.describe Ride, type: :model do
 
     it 'raises Exception if target_latitude is not within [-90,90]' do
         r = create(:ride)
-        expect{r.target_latitude = '91'}.to raise_error Exception, 'target_latitude is out of range'
+        expect{r.target_latitude = '91'}.to raise_error Exception, 'target_latitude out of range'
     end
     
     it 'raises Exception if source_longitude is not within [-180,180]' do
         r = create(:ride)
-        expect{r.source_longitude = '181'}.to raise_error Exception, 'source_longitude is out of range'
+        expect{r.source_longitude = '181'}.to raise_error Exception, 'source_longitude out of range'
     end
 
     it 'raises Exception if target_longitude is not within [-180,180]' do
         r = create(:ride)
-        expect{r.target_longitude = '181'}.to raise_error Exception, 'target_longitude is out of range'
+        expect{r.target_longitude = '181'}.to raise_error Exception, 'target_longitude out of range'
     end
 end
