11c903f89f125760a6c94f5e6769829d3871e3bf
Hasan Diwan <hd1@jsc.d8u.us>
2021-10-12 17:17:36 -0700

    add default root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'uber_request/new'
   get 'uber_request/arrived'
   devise_for :users
+  root to: 'uber_request#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
